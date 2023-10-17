31805bf476ed97e3cbd8944f76798ffd6c1f4312
MaxWu <EfiPy.Core@gmail.com>
2023-10-17 22:37:18 +0800

    1. EfiPy2.Lib package. 2. New EfiPy2.Lib.CpuId module. 3. New CpuId example code CpuIdBasic.py.

---
+++ Efi/Apps/EfiPy2Sample/CpuIdBasic.py
@@ -0,0 +1,148 @@
+# CpuId.py
+#
+# EfiPy2.Lib.CpuId
+#   part of EfiPy2
+#
+# Copyright (C) 2023 MaxWu efipy.core@gmail.com
+#   GPL-2.0
+#
+
+import EfiPy2 as EfiPy
+import EfiPy2.MdePkg.Register.Intel.Cpuid as CpuidRegs
+from EfiPy2.Lib import CpuId
+
+CpuIdObj  = CpuId.CpuIdClass()
+
+class CpuIdRegisters (EfiPy.Structure):
+  _pack_   = 1
+  _fields_ = [
+    ('EAX',  EfiPy.UINT32),
+    ('EBX',  EfiPy.UINT32),
+    ('ECX',  EfiPy.UINT32),
+    ('EDX',  EfiPy.UINT32)
+  ]
+
+CpuIdReg  = CpuIdRegisters()
+
+rax = CpuIdObj.GetId2 (0, 0, CpuIdReg)
+
+print (f'''
+CPU signature From cpuid instruction
+============================================
+  EAX = 0x{CpuIdReg.EAX:08X}
+  EBX = 0x{CpuIdReg.EBX:08X}
+  ECX = 0x{CpuIdReg.ECX:08X}
+  EDX = 0x{CpuIdReg.EDX:08X}
+''')
+
+print (f'''
+CPU signature from pre-defined constant
+============================================
+  EBX = 0x{CpuidRegs.CPUID_SIGNATURE_GENUINE_INTEL_EBX:08X}
+  ECX = 0x{CpuidRegs.CPUID_SIGNATURE_GENUINE_INTEL_ECX:08X}
+  EDX = 0x{CpuidRegs.CPUID_SIGNATURE_GENUINE_INTEL_EDX:08X}
+''')
+
+class CpuIdRegisters (EfiPy.Structure):
+  _pack_   = 1
+  _fields_ = [
+    ('EAX',  CpuidRegs.CPUID_VERSION_INFO_EAX),
+    ('EBX',  CpuidRegs.CPUID_VERSION_INFO_EBX),
+    ('ECX',  CpuidRegs.CPUID_VERSION_INFO_ECX),
+    ('EDX',  CpuidRegs.CPUID_VERSION_INFO_EDX)
+  ]
+CpuIdReg  = CpuIdRegisters()
+
+rax = CpuIdObj.GetId2 (CpuidRegs.CPUID_VERSION_INFO, 0, CpuIdReg)
+
+print (f'''
+CPU version information in UINT32 format
+============================================
+  EAX = 0x{CpuIdReg.EAX.Uint32:08X}
+  EBX = 0x{CpuIdReg.EBX.Uint32:08X}
+  ECX = 0x{CpuIdReg.ECX.Uint32:08X}
+  EDX = 0x{CpuIdReg.EDX.Uint32:08X}
+''')
+
+print (f'''
+CPU version information in bit field format
+============================================
+  CpuIdReg.EAX.Bits.SteppingId:           {CpuIdReg.EAX.Bits.SteppingId}
+  CpuIdReg.EAX.Bits.Model:                {CpuIdReg.EAX.Bits.Model}
+  CpuIdReg.EAX.Bits.FamilyId:             {CpuIdReg.EAX.Bits.FamilyId}
+  CpuIdReg.EAX.Bits.ProcessorType:        {CpuIdReg.EAX.Bits.ProcessorType}
+  CpuIdReg.EAX.Bits.Reserved1:            {CpuIdReg.EAX.Bits.Reserved1}
+  CpuIdReg.EAX.Bits.ExtendedModelId:      {CpuIdReg.EAX.Bits.ExtendedModelId}
+  CpuIdReg.EAX.Bits.ExtendedFamilyId:     {CpuIdReg.EAX.Bits.ExtendedFamilyId}
+  CpuIdReg.EAX.Bits.Reserved2:            {CpuIdReg.EAX.Bits.Reserved2}
+
+  CpuIdReg.EBX.Bits.BrandIndex:           {CpuIdReg.EBX.Bits.BrandIndex}
+  CpuIdReg.EBX.Bits.CacheLineSize:        {CpuIdReg.EBX.Bits.CacheLineSize}
+  CpuIdReg.EBX.Bits.MaximumAddressableIdsForLogicalProcessors: {CpuIdReg.EBX.Bits.MaximumAddressableIdsForLogicalProcessors}
+  CpuIdReg.EBX.Bits.InitialLocalApicId:   {CpuIdReg.EBX.Bits.InitialLocalApicId}
+
+  CpuIdReg.ECX.Bits.SSE3:                 {CpuIdReg.ECX.Bits.SSE3}
+  CpuIdReg.ECX.Bits.PCLMULQDQ:            {CpuIdReg.ECX.Bits.PCLMULQDQ}
+  CpuIdReg.ECX.Bits.DTES64:               {CpuIdReg.ECX.Bits.DTES64}
+  CpuIdReg.ECX.Bits.MONITOR:              {CpuIdReg.ECX.Bits.MONITOR}
+  CpuIdReg.ECX.Bits.DS_CPL:               {CpuIdReg.ECX.Bits.DS_CPL}
+  CpuIdReg.ECX.Bits.VMX:                  {CpuIdReg.ECX.Bits.VMX}
+  CpuIdReg.ECX.Bits.SMX:                  {CpuIdReg.ECX.Bits.SMX}
+  CpuIdReg.ECX.Bits.EIST:                 {CpuIdReg.ECX.Bits.EIST}
+  CpuIdReg.ECX.Bits.TM2:                  {CpuIdReg.ECX.Bits.TM2}
+  CpuIdReg.ECX.Bits.SSSE3:                {CpuIdReg.ECX.Bits.SSSE3}
+  CpuIdReg.ECX.Bits.CNXT_ID:              {CpuIdReg.ECX.Bits.CNXT_ID}
+  CpuIdReg.ECX.Bits.FMA:                  {CpuIdReg.ECX.Bits.FMA}
+  CpuIdReg.ECX.Bits.CMPXCHG16B:           {CpuIdReg.ECX.Bits.CMPXCHG16B}
+  CpuIdReg.ECX.Bits.xTPR_Update_Control:  {CpuIdReg.ECX.Bits.xTPR_Update_Control}
+  CpuIdReg.ECX.Bits.PDCM:                 {CpuIdReg.ECX.Bits.PDCM}
+  CpuIdReg.ECX.Bits.Reserved:             {CpuIdReg.ECX.Bits.Reserved}
+  CpuIdReg.ECX.Bits.PCID:                 {CpuIdReg.ECX.Bits.PCID}
+  CpuIdReg.ECX.Bits.DCA:                  {CpuIdReg.ECX.Bits.DCA}
+  CpuIdReg.ECX.Bits.SSE4_1:               {CpuIdReg.ECX.Bits.SSE4_1}
+  CpuIdReg.ECX.Bits.SSE4_2:               {CpuIdReg.ECX.Bits.SSE4_2}
+  CpuIdReg.ECX.Bits.x2APIC:               {CpuIdReg.ECX.Bits.x2APIC}
+  CpuIdReg.ECX.Bits.MOVBE:                {CpuIdReg.ECX.Bits.MOVBE}
+  CpuIdReg.ECX.Bits.POPCNT:               {CpuIdReg.ECX.Bits.POPCNT}
+  CpuIdReg.ECX.Bits.TSC_Deadline:         {CpuIdReg.ECX.Bits.TSC_Deadline}
+  CpuIdReg.ECX.Bits.AESNI:                {CpuIdReg.ECX.Bits.AESNI}
+  CpuIdReg.ECX.Bits.XSAVE:                {CpuIdReg.ECX.Bits.XSAVE}
+  CpuIdReg.ECX.Bits.OSXSAVE:              {CpuIdReg.ECX.Bits.OSXSAVE}
+  CpuIdReg.ECX.Bits.AVX:                  {CpuIdReg.ECX.Bits.AVX}
+  CpuIdReg.ECX.Bits.F16C:                 {CpuIdReg.ECX.Bits.F16C}
+  CpuIdReg.ECX.Bits.RDRAND:               {CpuIdReg.ECX.Bits.RDRAND}
+  CpuIdReg.ECX.Bits.ParaVirtualized:      {CpuIdReg.ECX.Bits.ParaVirtualized}
+
+  CpuIdReg.EDX.Bits.FPU:                  {CpuIdReg.EDX.Bits.FPU}
+  CpuIdReg.EDX.Bits.VME:                  {CpuIdReg.EDX.Bits.VME}
+  CpuIdReg.EDX.Bits.DE:                   {CpuIdReg.EDX.Bits.DE}
+  CpuIdReg.EDX.Bits.PSE:                  {CpuIdReg.EDX.Bits.PSE}
+  CpuIdReg.EDX.Bits.TSC:                  {CpuIdReg.EDX.Bits.TSC}
+  CpuIdReg.EDX.Bits.MSR:                  {CpuIdReg.EDX.Bits.MSR}
+  CpuIdReg.EDX.Bits.PAE:                  {CpuIdReg.EDX.Bits.PAE}
+  CpuIdReg.EDX.Bits.MCE:                  {CpuIdReg.EDX.Bits.MCE}
+  CpuIdReg.EDX.Bits.CX8:                  {CpuIdReg.EDX.Bits.CX8}
+  CpuIdReg.EDX.Bits.APIC:                 {CpuIdReg.EDX.Bits.APIC}
+  CpuIdReg.EDX.Bits.Reserved1:            {CpuIdReg.EDX.Bits.Reserved1}
+  CpuIdReg.EDX.Bits.SEP:                  {CpuIdReg.EDX.Bits.SEP}
+  CpuIdReg.EDX.Bits.MTRR:                 {CpuIdReg.EDX.Bits.MTRR}
+  CpuIdReg.EDX.Bits.PGE:                  {CpuIdReg.EDX.Bits.PGE}
+  CpuIdReg.EDX.Bits.MCA:                  {CpuIdReg.EDX.Bits.MCA}
+  CpuIdReg.EDX.Bits.CMOV:                 {CpuIdReg.EDX.Bits.CMOV}
+  CpuIdReg.EDX.Bits.PAT:                  {CpuIdReg.EDX.Bits.PAT}
+  CpuIdReg.EDX.Bits.PSE_36:               {CpuIdReg.EDX.Bits.PSE_36}
+  CpuIdReg.EDX.Bits.PSN:                  {CpuIdReg.EDX.Bits.PSN}
+  CpuIdReg.EDX.Bits.CLFSH:                {CpuIdReg.EDX.Bits.CLFSH}
+  CpuIdReg.EDX.Bits.Reserved2:            {CpuIdReg.EDX.Bits.Reserved2}
+  CpuIdReg.EDX.Bits.DS:                   {CpuIdReg.EDX.Bits.DS}
+  CpuIdReg.EDX.Bits.ACPI:                 {CpuIdReg.EDX.Bits.ACPI}
+  CpuIdReg.EDX.Bits.MMX:                  {CpuIdReg.EDX.Bits.MMX}
+  CpuIdReg.EDX.Bits.FXSR:                 {CpuIdReg.EDX.Bits.FXSR}
+  CpuIdReg.EDX.Bits.SSE:                  {CpuIdReg.EDX.Bits.SSE}
+  CpuIdReg.EDX.Bits.SSE2:                 {CpuIdReg.EDX.Bits.SSE2}
+  CpuIdReg.EDX.Bits.SS:                   {CpuIdReg.EDX.Bits.SS}
+  CpuIdReg.EDX.Bits.HTT:                  {CpuIdReg.EDX.Bits.HTT}
+  CpuIdReg.EDX.Bits.TM:                   {CpuIdReg.EDX.Bits.TM}
+  CpuIdReg.EDX.Bits.Reserved3:            {CpuIdReg.EDX.Bits.Reserved3}
+  CpuIdReg.EDX.Bits.PBE:                  {CpuIdReg.EDX.Bits.PBE}
+''')
---
+++ Efi/StdLib/lib/python36.8/EfiPy2/Lib/CpuId.py
@@ -0,0 +1,87 @@
+# CpuId.py
+#
+# EfiPy2.Lib.CpuId
+#   part of EfiPy2
+#
+# Copyright (C) 2023 MaxWu efipy.core@gmail.com
+#   GPL-2.0
+#
+
+import EfiPy2 as EfiPy
+
+class CpuIdClass:
+
+  def __init__ (self):
+
+    import corepy.arch.x86_64.isa as x86
+    import corepy.arch.x86_64.types.registers as reg
+    import corepy.arch.x86_64.platform as env
+    import corepy.arch.x86_64.lib.memory as mem
+
+    self.code   = env.InstructionStream()
+    self.proc   = env.Processor()
+    self.params = env.ExecParams()
+
+    self.code.add(x86.mov(reg.rax, mem.MemRef(reg.rbp, 16)))  # parameter 1
+    self.code.add(x86.mov(reg.rcx, mem.MemRef(reg.rbp, 24)))  # parameter 2
+    self.code.add(x86.mov(reg.rdi, mem.MemRef(reg.rbp, 32)))  # parameter 3
+
+    self.code.add(x86.push(reg.rax))                          # save input parameter
+
+    self.code.add(x86.cpuid())
+
+    self.code.add(x86.mov(mem.MemRef(reg.rdi,  0, data_size = 32), reg.eax))
+    self.code.add(x86.mov(mem.MemRef(reg.rdi,  4, data_size = 32), reg.ebx))
+    self.code.add(x86.mov(mem.MemRef(reg.rdi,  8, data_size = 32), reg.ecx))
+    self.code.add(x86.mov(mem.MemRef(reg.rdi, 12, data_size = 32), reg.edx))
+
+    self.code.add(x86.pop(reg.rax))                           # restore input parameter as return value
+
+  #
+  # UINT32 GetId (UINT32 eax, UINT32 ecx, CpuIdRegisters *CpuIdRegAddr);
+  #
+  def GetId (self, eax, ecx, CpuIdRegAddr):
+
+    self.params.p1 = eax
+    self.params.p2 = ecx
+    self.params.p3 = CpuIdRegAddr
+    return self.proc.execute(self.code, params = self.params, mode = 'int')
+
+  #
+  # UINT32 GetId (UINT32 eax, UINT32 ecx, struct CpuIdReg &CpuIdReg);
+  #
+  def GetId2 (self, eax, ecx, CpuIdReg):
+
+    CpuIdRegAddr = EfiPy.addressof (CpuIdReg)
+
+    self.params.p1 = eax
+    self.params.p2 = ecx
+    self.params.p3 = CpuIdRegAddr
+    return self.proc.execute(self.code, params = self.params, mode = 'int')
+
+if __name__ == '__main__':
+
+  import EfiPy2.MdePkg.Register.Intel.Cpuid as CpuidRegs
+
+  class CpuIdRegisters (EfiPy.Structure):
+    _pack_   = 1
+    _fields_ = [
+      ('EAX',  CpuidRegs.CPUID_VERSION_INFO_EAX),
+      ('EBX',  CpuidRegs.CPUID_VERSION_INFO_EBX),
+      ('ECX',  CpuidRegs.CPUID_VERSION_INFO_ECX),
+      ('EDX',  CpuidRegs.CPUID_VERSION_INFO_EDX)
+    ]
+  CpuIdReg  = CpuIdRegisters()
+  CpuIdObj  = CpuIdClass()
+
+  rax = CpuIdObj.GetId2(CpuidRegs.CPUID_VERSION_INFO, 0, CpuIdReg)
+  
+
+  print (f'''
+  CPUID From instruction cpuid
+  ============================================
+    EAX = 0x{CpuIdReg.EAX.Uint32:08X}
+    EBX = 0x{CpuIdReg.EBX.Uint32:08X}
+    ECX = 0x{CpuIdReg.ECX.Uint32:08X}
+    EDX = 0x{CpuIdReg.EDX.Uint32:08X}
+  ''')
