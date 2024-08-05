94541da96a0295db731064bf4a29fcc9bb2a65f5
MaxWu <EfiPy.Core@gmail.com>
2024-08-05 22:43:29 +0800

    Modify Hexdump sample look and feel

--- Efi/Apps/EfiPy2Sample/HexDump.py
+++ Efi/Apps/EfiPy2Sample/HexDump.py
@@ -23,9 +23,11 @@ def HexDump (InputData, DumpHead = 0, OutFile = sys.stdout):
 def HexDumpDemo ():
 
   InputData = b'\x01\x02ab'
+  print (f'Try to dump binary data via HexDump {InputData}\n')
   HexDump (InputData)
   print()
   InputData = b'\x01\x02abcded\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x00\x0D\x0E\x0F\x10\x11\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04abcded\xaa'
+  print (f'Try to dump binary data via HexDump {InputData}\n')
   HexDump (InputData)
 
 if __name__ == '__main__':
