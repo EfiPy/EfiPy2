951c81b7aee81376eacb5c4bee42cbe6f5cb46d0
MaxWu <EfiPy.Core@gmail.com>
2024-08-21 21:27:59 +0800

    HexDump, for friendly API parameter

--- Efi/Apps/EfiPy2Sample/HexDump.py
+++ Efi/Apps/EfiPy2Sample/HexDump.py
@@ -1,24 +1,31 @@
 import sys
 
-def HexDump (InputData, DumpHead = 0, OutFile = sys.stdout):
+def HexDump (InputData, HexOffset = 0, DumpLead = 0, OutFile = sys.stdout):
 
-  InputArray = [InputData[i: i + 16] for i in range(0, len(InputData), 16)]
+  DumpEmpty  = HexOffset % 0x10
+  DumpData   = b' ' * DumpEmpty + InputData
+  DumpOffset = (HexOffset // 0x10) * 0x10
+  InputArray = [DumpData[i: i + 16] for i in range(0, len(DumpData), 16)]
 
-  print (f'            0  1  2  3  4  5  6  7   8  9  A  B  C  D  E  F  01234567 89ABCDEF', file = OutFile)
-  print (f'------------------------------------------------------------------------------', file = OutFile)
+  print (' ' * DumpLead + f'            0  1  2  3  4  5  6  7   8  9  A  B  C  D  E  F  01234567 89ABCDEF', file = OutFile)
+  print (' ' * DumpLead + f'------------------------------------------------------------------------------', file = OutFile)
   
   for BinaryData in InputArray:
 
     b = ''.join(f'{b:02X} ' for b in BinaryData)
     b = b + '   ' * (16 - len(BinaryData))
     b = b[:24] + ' ' + b[24:]
-
+    if DumpEmpty != 0:
+      e = '   ' * DumpEmpty
+      e += ' ' if DumpEmpty > 8 else ''
+      b = e + b[len(e):]
+    DumpEmpty = 0
     s = ''.join(f'{b:c}' if b >= 0x20 and b <= 0x7E else '.' for b in BinaryData)
     s = s + ' ' * (16 - len(BinaryData))
     s = s[:8] + ' ' + s[8:]
 
-    print(f'{DumpHead:08X}   {b} {s}', file = OutFile)
-    DumpHead += 16
+    print(' ' * DumpLead + f'{DumpOffset:08X}   {b} {s}', file = OutFile)
+    DumpOffset += 16
 
 def HexDumpDemo ():
 
@@ -28,7 +35,12 @@ def HexDumpDemo ():
   print()
   InputData = b'\x01\x02abcded\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x00\x0D\x0E\x0F\x10\x11\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04abcded\xaa'
   print (f'Try to dump binary data via HexDump {InputData}\n')
-  HexDump (InputData)
+  HexDump (InputData, HexOffset = 0x1000, DumpLead = 4)
+  print ()
+  HexDump (InputData, HexOffset = 0x1002, DumpLead = 4)
+  print ()
+  HexDump (InputData, HexOffset = 0x100A, DumpLead = 4)
+  print ()
 
 if __name__ == '__main__':
 
