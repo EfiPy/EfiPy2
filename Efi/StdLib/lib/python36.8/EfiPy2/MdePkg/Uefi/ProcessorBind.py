809ac617ffb6b54f672a7daeb724f71cdcc321cb
MaxWu <EfiPy.Core@gmail.com>
2024-08-24 06:53:00 +0800

    add ctypes big endian type

--- Efi/StdLib/lib/python36.8/EfiPy2/MdePkg/Uefi/ProcessorBind.py
+++ Efi/StdLib/lib/python36.8/EfiPy2/MdePkg/Uefi/ProcessorBind.py
@@ -8,18 +8,30 @@
 #
 from ctypes   import *
 
+UINTN   = c_uint64
+INTN    = c_int64
 UINT64  = c_uint64
 INT64   = c_int64
 UINT32  = c_uint32
 INT32   = c_int32
 UINT16  = c_uint16
+INT16   = c_int16
+UINT8   = c_uint8
+INT8    = c_int8
+BOOLEAN = c_uint8
 CHAR16  = c_wchar
 PCHAR16 = c_wchar_p
-INT16   = c_int16
-BOOLEAN = c_uint8
-UINT8   = c_uint8
 CHAR8   = c_char
 PCHAR8  = c_char_p
-INT8    = c_int8
-UINTN   = c_uint64
-INTN    = c_int64
+
+UINTN_BE   = c_uint64.__ctype_be__
+INTN_BE    = c_int64.__ctype_be__
+UINT64_BE  = c_uint64.__ctype_be__
+INT64_BE   = c_int64.__ctype_be__
+UINT32_BE  = c_uint32.__ctype_be__
+INT32_BE   = c_int32.__ctype_be__
+UINT16_BE  = c_uint16.__ctype_be__
+INT16_BE   = c_int16.__ctype_be__
+UINT8_BE   = c_uint8.__ctype_be__
+INT8_BE    = c_int8.__ctype_be__
+BOOLEAN_BE = c_uint8.__ctype_be__
