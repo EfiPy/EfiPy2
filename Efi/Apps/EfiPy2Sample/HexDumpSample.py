b9bad87f6856d6be74f66b88f78a2b025187e3c9
MaxWu <EfiPy.Core@gmail.com>
2025-02-04 22:48:27 +0800

    Add file headers and modify HexDump sample code more readable

--- Efi/Apps/EfiPy2Sample/HexDumpSample.py
+++ Efi/Apps/EfiPy2Sample/HexDumpSample.py
@@ -1,19 +1,29 @@
+# HexDumpSample.py
+#
+#   part of EfiPy2
+#
+# Copyright (C) 2025 MaxWu efipy.core@gmail.com
+#   GPL-2.0
+#
 from EfiPy2.Lib.HexDump import HexDump
 
 def HexDumpDemo ():
 
+  print("\n1. =======================================================================================")
   InputData = b'\x01\x02ab'
   print (f'Try to dump binary data via HexDump {InputData}\n')
   HexDump (InputData)
-  print()
+
+  print("\n2. =======================================================================================")
   InputData = b'\x01\x02abcded\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x00\x0D\x0E\x0F\x10\x11\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04abcded\xaa'
   print (f'Try to dump binary data via HexDump {InputData}\n')
   HexDump (InputData, HexOffset = 0x1000, DumpLead = 4)
-  print ()
+
+  print ("\n3. =======================================================================================")
   HexDump (InputData, HexOffset = 0x1002, DumpLead = 4)
-  print ()
+
+  print ("\n4. =======================================================================================")
   HexDump (InputData, HexOffset = 0x100A, DumpLead = 4)
-  print ()
 
 if __name__ == '__main__':
 
@@ -22,6 +32,11 @@ if __name__ == '__main__':
     HexDumpDemo ()
     sys.exit(0)
 
-  with open (sys.argv[1], 'br') as f:
-    b = f.read()
-    HexDump (b)
+  try:
+    with open (sys.argv[1], 'br') as f:
+      b = f.read()
+      HexDump (b)
+  except FileNotFoundError as e:
+    print (e)
+    print ("Turn to internal default demo program...")
+    HexDumpDemo ()
