b55ae21bad8c24b6aadf01d1d189c339b426e8fd
MaxWu <EfiPy.Core@gmail.com>
2025-02-05 22:20:41 +0800

    HexDump democe code to fit 80x25 terminal display

--- Efi/StdLib/lib/python36.8/EfiPy2/Lib/HexDump.py
+++ Efi/StdLib/lib/python36.8/EfiPy2/Lib/HexDump.py
@@ -37,21 +37,21 @@ def HexDump (InputData, HexOffset = 0, DumpLead = 0, OutFile = sys.stdout):
 
 def HexDumpDemo ():
 
-  print("\n1. =======================================================================================")
+  print("\n1. =============================================================================")
   InputData = b'\x01\x02ab'
   print (f'Try to dump binary data via HexDump {InputData}\n')
-  HexDump (InputData)
+  HexDump (InputData, DumpLead = 1)
 
-  print("\n2. =======================================================================================")
+  print("\n2. =============================================================================")
   InputData = b'\x01\x02abcded\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x00\x0D\x0E\x0F\x10\x11\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04\x04abcded\xaa'
   print (f'Try to dump binary data via HexDump {InputData}\n')
-  HexDump (InputData, HexOffset = 0x1000, DumpLead = 4)
+  HexDump (InputData, HexOffset = 0x1000, DumpLead = 1)
 
-  print ("\n3. =======================================================================================")
-  HexDump (InputData, HexOffset = 0x1002, DumpLead = 4)
+  print ("\n3. ============================================================================")
+  HexDump (InputData, HexOffset = 0x1002, DumpLead = 1)
 
-  print ("\n4. =======================================================================================")
-  HexDump (InputData, HexOffset = 0x100A, DumpLead = 4)
+  print ("\n4. ============================================================================")
+  HexDump (InputData, HexOffset = 0x100A, DumpLead = 1)
 
 if __name__ == '__main__':
 
