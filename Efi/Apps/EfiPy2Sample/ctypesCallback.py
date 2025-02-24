dc73baae55b73d6c301298f65bf15bc4551703e7
MaxWu <EfiPy.Core@gmail.com>
2025-02-24 19:36:25 +0800

    patch copyright in ctypesCallback.py

--- Efi/Apps/EfiPy2Sample/ctypesCallback.py
+++ Efi/Apps/EfiPy2Sample/ctypesCallback.py
@@ -1,3 +1,20 @@
+# ctypesCallback.py
+#
+# Copyright (C) 2024 - 2025 efipy.core@gmail.com All rights reserved.
+#
+# asm.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# PaTest.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EfiPy.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import ctypes
 
 def EfiPy2cFunc1 (Val1, Val2):
