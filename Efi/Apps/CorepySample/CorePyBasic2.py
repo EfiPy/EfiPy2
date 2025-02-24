17a89b2bbb3b0f005609d13c2afbcd884d3df68b
MaxWu <EfiPy.Core@gmail.com>
2025-02-24 21:51:49 +0800

    sample code for calling corepy machine code stream via ctype CFUNCTION

---
+++ Efi/Apps/CorepySample/CorePyBasic2.py
@@ -0,0 +1,55 @@
+# CorePyBasic2.py
+#
+# Copyright (C) 2025 efipy.core@gmail.com All rights reserved.
+#
+# asm.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# PaTest.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EfiPy.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+def CorePyAdd (a: int, b: int, DumpCode: bool = False) -> int:
+    import corepy.arch.x86_64.isa as x86
+    import corepy.arch.x86_64.types.registers as reg
+    import corepy.arch.x86_64.lib.memory as mem
+    import corepy.arch.x86_64.platform as env
+
+    code    = env.InstructionStream()
+
+    code.add(x86.mov(reg.rax, mem.MemRef(reg.rbp, 16)))  # ret = a
+    code.add(x86.add(reg.rax, mem.MemRef(reg.rbp, 24)))  # ret = ret + b
+
+    if DumpCode:
+        code.print_code(pro = True, epi = True, hex = True)
+
+    CodeAddr, CodeBytes = code.get_code_bytes ()
+
+    import EfiPy2 as EfiPy
+
+    CorePy2EfiPyType = EfiPy.CFUNCTYPE (
+      EfiPy.UINT32,     # Retturn Value
+      EfiPy.UINT32,     # Parameter 1
+      EfiPy.UINT32      # Parameter 2
+      )
+
+    CorePy2EfiPyFunc = CorePy2EfiPyType(CodeAddr)
+
+    ret = CorePy2EfiPyFunc (a, b)
+    return ret
+
+if __name__ == '__main__':
+
+    input1 = 100
+    input2 = 200
+
+    ret1 = CorePyAdd (input1, input2)   # ret1 = input1 + input2
+    ret2 = input1 + input2              # ret2 = input1 + input2
+
+    print (f'{input1} + {input2} = {ret1}, result: {ret1 == ret2}')
